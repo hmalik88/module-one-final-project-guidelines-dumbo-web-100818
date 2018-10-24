a635fd7441a017fc0e68097771ad163c2ab6ff94
CarsonRey <c.rey494@gmail.com>
2018-10-24 16:48:38 -0400

    Fixed issue where FoodRec to user included repeat foods

--- bin/run.rb
+++ bin/run.rb
@@ -1,4 +1,5 @@
 require_relative '../config/environment'
 
+
 welcome_message
 returning_user?
--- lib/food_methods.rb
+++ lib/food_methods.rb
@@ -1,13 +1,15 @@
-def low_sodium_rec
-  food_array = Food.where("sodium_level = ?", "low")
-  food_array.sample(5)
+def low_sodium
+  Food.where("sodium_level = ?", "low").shuffle
 end
 
 def high_sodium_rec
-  food_array = Food.where("sodium_level = ?", "high")
-  food_array.sample(5).uniq
+  Food.where("sodium_level = ?", "high")
 end
 
+
+def low_sodium_rec
+    arr = low_sodium.map(&:name).sample(5).uniq
+end
 
 def food_array
   puts FoodRec.last.food_rec1
@@ -20,8 +22,8 @@ def food_array
 end
 
 def give_user_low_sodium_food_recs
-  FoodRec.create(user_id: current_user.user_id, food_rec1: low_sodium_rec[0].name ,food_rec2: low_sodium_rec[1].name, food_rec3: low_sodium_rec[2].name, food_rec4: low_sodium_rec[3].name,food_rec5: low_sodium_rec[4].name)
-
+  FoodRec.create(user_id: current_user.user_id, food_rec1: low_sodium_rec[0],food_rec2: low_sodium_rec[1], food_rec3: low_sodium_rec[2], food_rec4: low_sodium_rec[3],food_rec5: low_sodium_rec[4])
+  
   food_array
 end
 
